3c06a9e123d71a419866ab44cd70bcffbe0efe21
omersade-10 <65088341+omersade-10@users.noreply.github.com>
2022-10-24 16:53:29 +0300

    Add MongoDB and mongoose

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 4000;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/productsdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
