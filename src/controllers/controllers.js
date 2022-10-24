18b830fbef97b4e5eb93f3a423aedfbc5477c46c
omersade-10 <65088341+omersade-10@users.noreply.github.com>
2022-10-24 17:36:27 +0300

    Basic CRUD endpoints

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const { ProductSchema } = require('../models/models');
+import mongoose from 'mongoose';
+import { ProductSchema } from '../models/models';
 
 const Product = mongoose.model('Product', ProductSchema);
 
@@ -7,6 +7,24 @@ export const addnewProduct = (req, res) => {
     let newProduct = new Product(req.body);
 
     newProduct.save((err, Product) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(Product);
+    });
+}
+
+export const getProducts = (req, res) => {
+    Product.find({}, (err, Product) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(Product);
+    });
+}
+
+export const getProductWithID = (req, res) => {
+    Product.findById(req.params.ProductID, (err, Product) => {
         if (err) {
             res.send(err);
         }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,18 @@
-import { addnewProduct } from "../controllers/controllers";
+import { addnewProduct,
+    getProducts,
+    getProductWithID
+} from '../controllers/controllers';
 
-export function routes(app) {
-    app.route('/products').post(addnewProduct);
+const routes = (app) => {
+app.route('/products')
+    .get(getProducts)
+    
+    // Post endpoint
+    .post(addnewProduct);
+
+app.route('/products/:ProductID')
+    // get a specific Product
+    .get(getProductWithID)
 }
+
+export default routes;
